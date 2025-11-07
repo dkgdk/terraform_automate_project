97a74338492ebc54c3c6191fe329550c87494a0c
dkgdk <128899645+dkgdk@users.noreply.github.com>
2025-11-07 22:55:56 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,5 +1,8 @@
 terraform {
   required_providers {
+    terraform {
+      backend "s3" {}
+    }
     aws = { source = "hashicorp/aws" }
     tls = { source = "hashicorp/tls" }
     random = { source = "hashicorp/random" }
