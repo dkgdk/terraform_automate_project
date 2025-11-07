8d4788184e2916d6de8bca010af4958dd1864217
dkgdk <128899645+dkgdk@users.noreply.github.com>
2025-11-07 21:56:59 +0530

    Update outputs.tf

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,2 +1,19 @@
-output "instance_public_ip" { value = aws_instance.ec2.public_ip }
-output "instance_id" { value = aws_instance.ec2.id }
+output "instance_id" {
+  description = "The EC2 instance ID"
+  value       = aws_instance.example.id
+}
+
+output "public_ip" {
+  description = "The EC2 instance public IP"
+  value       = aws_instance.example.public_ip
+}
+
+output "private_ip" {
+  description = "The EC2 instance private IP"
+  value       = aws_instance.example.private_ip
+}
+
+output "key_name" {
+  description = "The EC2 key pair name"
+  value       = aws_key_pair.deployer_key.key_name
+}
