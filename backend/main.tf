ec41130747ff3e0198baed813f27ae1f6d428427
dkgdk <128899645+dkgdk@users.noreply.github.com>
2025-11-07 22:54:14 +0530

    Update main.tf

--- backend/main.tf
+++ backend/main.tf
@@ -32,17 +32,6 @@ resource "aws_s3_bucket" "tf_state" {
   }
 }
 
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = "${var.backend_bucket_name}-tf-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "tf-lock-table"
-  }
+output "backend_bucket_name" {
+  value = aws_s3_bucket.tf_state.bucket
 }
